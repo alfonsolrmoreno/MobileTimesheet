17109adea9d4b244b0248e7027db2adf746775de
alfonsolrmoreno <alfonso.lr.moreno@gmail.com>
2013-10-30 14:39:07 -0200

    v22
    endereço vmulti.mobile e correções

--- default/js/default.js
+++ default/js/default.js
@@ -1,40 +1,33 @@
-var COMMON_URL_MOBILE = 'http://www.multidadosti.com/fgv/mobile/';
+var COMMON_URL_MOBILE = 'http://www.multidadosti.com/vmulti.mobile/mobile/';
 
 function salvar_timesheet()
 {
 	var dados = new Object();
-	dados['USUARIO_WS'] = 'multidados';
-	dados['SENHA_WS'] = 'multi';
-	dados['CNPJ_EMPRESA'] = '00.000.000/0000-00';
-	dados['CODIGO_AUXILIAR_PREST'] = '0100';
-	dados['DATA_LANCAMENTO'] = $("#data_trabalhada").val();
-	dados['CODIGO_AUXILIAR_CLI'] = $("#codigo_auxiliar").val();
-	dados['CODIGO_PROJETO'] = $("#codigo").val();
-	dados['CODIGO_FASE'] = $("#utbms_fase").val();
-	dados['COD_ATIVIDADE'] = $("#utbms_atividade").val();
-	dados['HR_INICIO'] = $("#hora_inicial").val();
-	dados['HR_FIM'] = $("#hora_final").val();
-	dados['COBRAVEL_NAO_COBRAVEL'] = 'F';
+
+	alert('ssssssss');
+
+	dados['USUARIO_WS'] 				= 'multidados';
+	dados['SENHA_WS'] 					= 'multi';
+	dados['CNPJ_EMPRESA'] 				= '00.000.000/0000-00';
+	dados['CODIGO_AUXILIAR_PREST'] 		= '0100';
+	dados['DATA_LANCAMENTO'] 			= $("#data_trabalhada").val();
+	dados['CODIGO_AUXILIAR_CLI'] 		= $("#codigo_auxiliar").val();
+	dados['CODIGO_PROJETO'] 			= $("#codigo").val();
+	dados['CODIGO_FASE'] 				= $("#codigo_fase").val();
+	dados['COD_ATIVIDADE'] 				= $("#codigo_atividade").val();
+	dados['HR_INICIO'] 					= $("#hora_inicial").val();
+	dados['HR_FIM'] 					= $("#hora_final").val();
+	dados['COBRAVEL_NAO_COBRAVEL'] 		= 'F';
 
 	dados['NARRATIVA_PRINCIPAL'] = $("#narrativa_principal").val();
 	acao = 'DadosTimesheet';
 	operacao = 'cadastrar';
-	
+
 	var ajax_file = COMMON_URL_MOBILE+'client.php';
-	
-	/*
-	$.post(ajax_file, {dados: dados ,acao:acao,operacao:operacao}, function(resposta)
-	{
-		if(resposta == 'T')
-		{
-			alert("Registro gravado com sucesso!");
-		}
-		else
-		{
-			alert(resposta);
-		}
-	});
-	*/
+
+
+	alert('zzazazazazazaza');
+
 	$.ajax({
 		type : 'POST',
 		url: ajax_file,
@@ -49,6 +42,7 @@ function salvar_timesheet()
 	})
 	.then( function ( response )
 	{
+		alert(response);
 		if(response == 'T')
 		{
 			alert("Registro gravado com sucesso!");
@@ -58,12 +52,13 @@ function salvar_timesheet()
 			alert(response);
 		}
 	});
-	
+
 }
 
 function selecionaValor(valor,tipo,id,id2,nome2)
 {
 	$( ".ui-body-"+tipo ).val(valor);
+
 	if(tipo == 'c')
 	{
 		$( "#codigo_auxiliar" ).val(id);
@@ -73,7 +68,7 @@ function selecionaValor(valor,tipo,id,id2,nome2)
 	else if(tipo=='p')
 	{
 		$( "#codigo" ).val(id);
-		
+
 		if($( "#codigo_auxiliar" ).val() == '')
 		{
 			$( "#codigo_auxiliar" ).val(id2);
@@ -82,34 +77,35 @@ function selecionaValor(valor,tipo,id,id2,nome2)
 	}
 	else if(tipo=='t')
 	{
-		$( "#utbms_fase" ).val(id);
+		$( "#codigo_fase" ).val(id);
 	}
-	else if(tipo=='g')
+	else if(tipo=='atividade')
 	{
-		$( "#utbms_atividade" ).val(id);
+		$( "#codigo_atividade" ).val(id);
 	}
-	
+
 	$("ul").empty();
 }
 
-$( document ).on( "pageinit", "#page_timesheet", function() 
+$( document ).on( "pageinit", "#page_timesheet", function()
 {
-	$( "#autocomplete_cliente" ).on( "listviewbeforefilter", function ( e, data ) 
+	$( "#autocomplete_cliente" ).on( "listviewbeforefilter", function ( e, data )
 	{
 		var $ul = $( this ),
 			$input = $( data.input ),
 			value = $input.val(),
 			html = "";
 		$ul.html( "" );
-		
+
 		if ( value && value.length > 2 )
 		{
-			idclienteprojeto = $( "#codigo" ).val();
+			codigo_cliente = $( "#codigo_cliente" ).val();
+
 			$ul.html( "<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>" );
 			$ul.listview( "refresh" );
 			$.ajax({
 				type : 'GET',
-				url: COMMON_URL_MOBILE+"search.php?tipo=c&codigo="+idclienteprojeto,
+				url: COMMON_URL_MOBILE+"search.php?tipo=c&codigo="+codigo_cliente,
 				dataType: "jsonp",
 				//dataType: "json", // quando for interno deve habilitar este e ocultar datatype e crossDomain
 				crossDomain: true,
@@ -160,8 +156,18 @@ $( document ).on( "pageinit", "#page_timesheet", function()
 	});
 });
 
-$(document).ready(function () 
+$(document).ready(function ()
 {
+	$("#save_timecard_top").click(function()
+	{
+		salvar_timesheet();
+	});
+
+	$("#save_timecard_bottom").click(function()
+	{
+		salvar_timesheet();
+	});
+
 	$.ajax({
 		type : 'GET',
 		url: COMMON_URL_MOBILE+'search.php?tipo=t',
@@ -173,30 +179,30 @@ $(document).ready(function ()
 			q: $input.val()
 		}*/
 	})
-	.then( function ( response ) 
+	.then( function ( response )
 	{
 		var items = [];
-		var options = '<option value="">Escolha uma fase</option>';
+		var options = '<option value="">Selecione uma fase</option>';
 
 		$.each(response, function (key, val) {
-			
+
 			options += '<option value="' + val.utbms + '">' + val.utbms_nome + '</option>';
 		});
-		
-		$("#utbms_fase").html(options);		
+
+		$("#codigo_fase").html(options);
 	});
 
-	$( "#utbms_fase" ).change(function()
+	$( "#codigo_fase" ).change(function()
 	{
-		var options = '<option value="">Escolha uma atividade</option>';
-		$("#utbms_atividade").html(options);
-		val_idutbms_fase = $( "#utbms_fase" ).val()
-	    
-		if($( "#utbms_fase" ).val()!='')
-		{	
+		var options = '<option value="">Selecione uma atividade</option>';
+		$("#codigo_atividade").html(options);
+		val_idutbms_fase = $( "#codigo_fase" ).val()
+
+		if($( "#codigo_fase" ).val()!='')
+		{
 			$.ajax({
 				type : 'GET',
-				url: COMMON_URL_MOBILE+'search.php?tipo=g&idtarefa='+val_idutbms_fase,
+				url: COMMON_URL_MOBILE+'search.php?tipo=atividade&idtarefa='+val_idutbms_fase,
 				dataType: "jsonp",
 				//dataType: "json", // quando for interno deve habilitar este e ocultar datatype e crossDomain
 				crossDomain: true
@@ -205,7 +211,7 @@ $(document).ready(function ()
 					q: $input.val()
 				}*/
 			})
-			.then( function ( response ) 
+			.then( function ( response )
 			{
 				var items = [];
 				var options = '<option value="">Escolha uma atividade</option>';
@@ -213,8 +219,8 @@ $(document).ready(function ()
 				$.each(response, function (key, val) {
 					options += '<option value="' + val.idutbms + '">' + val.utbms_nome + '</option>';
 				});
-				
-				$("#utbms_atividade").html(options);
+
+				$("#codigo_atividade").html(options);
 			});
 	  }
 	});
