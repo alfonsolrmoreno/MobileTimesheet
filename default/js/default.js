a7b38b4b9563ab78132bc1934b1e8a2314b0b4a7
alfonsolrmoreno <alfonso.lr.moreno@gmail.com>
2015-10-05 12:20:58 -0300

    vs FINAL

--- default/js/default.js
+++ default/js/default.js
@@ -1,6 +1,6 @@
 //versao do mobile para mostrar no footer
 var vs_mobile = 'v.3.0.0';
-var debug_mode = true;
+var debug_mode = false;
 
 var Objeto_real = localStorage['mobile_login'];
 
@@ -412,6 +412,8 @@ function mobile_login(obj) {
                             }else{
                                 window.location.href = 'index.html';
                             }
+							
+							popMenuDash();
                         }
                     }
                 });
